1cf02a846a23d57a3ea68eadc7dc50a30d95fd6a
VaclavMatula <76968241+VaclavMatula@users.noreply.github.com>
2022-02-03 12:32:00 +0100

    Create Clicker-0.1.1.py

---
+++ Clicker-0.1.1.py
@@ -0,0 +1,32 @@
+#Python project - clicker
+import json
+
+def into_dict ():
+    global vars
+    for variable in ["points", "buil01", "achi01", "upgr01"]:
+        vars[variable] = eval(variable)
+
+def new_game():
+
+    global points
+    global buil01
+    global achi01
+    global upgr01
+    
+    points = 0
+    buil01 = 0
+    achi01 = 0
+    upgr01 = 0
+
+def into_file():
+    with open('clickersave.txt', 'w') as convert_file:
+        convert_file.write(json.dumps(vars))
+
+vars = {}
+
+new_game()
+into_dict()
+into_file()
+
+print(vars)
+print (vars['points'])
